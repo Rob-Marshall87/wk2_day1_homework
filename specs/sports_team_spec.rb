da2fc74fc949ae366b27f623f52441c42602eeb7
Rob-Marshall87 <marshallrobbie@hotmail.com>
2019-03-04 15:15:08 +0000

    add new player

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -18,4 +18,15 @@ class TestSportsTeam < MiniTest::Test
     assert_equal(["player1", "player2"], sports_team.players)
   end
 
+  def test_change_coach
+    sports_team = SportsTeam.new("Edinburgh", ["player1", "player2"], "Joe Smit")
+    sports_team.coach = "Vern Cotter"
+    assert_equal("Vern Cotter", sports_team.coach)
+  end
+
+  def test_add_new_player
+    sports_team = SportsTeam.new("Edinburgh", ["player1", "player2"], "Joe Smit")
+    sports_team.players += ["player3"]
+    assert_equal(["player1", "player2", "player3"], sports_team.players)
+  end
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,10 +1,14 @@
 class SportsTeam
-  attr_reader :team_name, :coach, :players
+  attr_reader :team_name
+  attr_accessor :coach, :players
   def initialize(team_name, players ,coach)
     @team_name = team_name
     @coach = coach
     @players = players
   end
 
+  # def add_new_player(player)
+  #
+  # end
 
 end
