76702b307c395219052f407755e920f64c521d86
Rob-Marshall87 <marshallrobbie@hotmail.com>
2019-03-04 22:27:12 +0000

    couldn't figure out where/how to fit the library class

--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -3,5 +3,8 @@ require('minitest/rg')
 require_relative('../library')
 
 class TestLibrary < MiniTest::Test
-  def 
+  def test_get_book
+    book = Library.new("The Hobbit", { "Rob", 03/07/17})
+    assert_equal(("The Hobbit", { "Rob", 03/07/17}), book)
+  end
 end
